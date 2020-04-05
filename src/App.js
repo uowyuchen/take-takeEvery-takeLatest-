f36f319151ae0072e0e34900aa9e2150ffb993fb
lizhen <uowyuchen@gmail.com>
2020-04-04 20:29:17 -0400

    The initial file of practicing take, takeEvery, takeLatest

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+import Card from "./card/card.component";
+
+import "./App.css";
+
+const App = (props) => {
+  const { increment, decrement, value } = props;
+  return (
+    <div className='App'>
+      <Card>
+        {value}
+        <button onClick={increment}>Add 1</button>
+        <button onClick={decrement}>Minus 1</button>
+      </Card>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  value: state.app.value,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch({ type: "INCREMENT" }),
+  decrement: () => dispatch({ type: "DECREMENT" }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
